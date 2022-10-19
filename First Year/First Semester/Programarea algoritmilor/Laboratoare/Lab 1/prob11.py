94c6a64aa72e64ab6f788443358596a59d75e220
Ilie-Daniel Apostol <47429017+apostoldaniel854@users.noreply.github.com>
2022-10-19 15:27:35 +0300

    Update prob11.py

--- First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob11.py
+++ First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob11.py
@@ -13,9 +13,7 @@ b = int(input("b="))
 ans = -1
 for x in range(a, b + 1):
     if prim(x) == True:
-        ans = x
+        print(f"Primul numar prim din [{a}, {b}] este", x)
         break
-if ans == -1:
+else:
     print(f"Nu exista niciun numar prin in [{a}, {b}]")
-else: 
-    print(f"Primul numar prim din [{a}, {b}] este", ans)
