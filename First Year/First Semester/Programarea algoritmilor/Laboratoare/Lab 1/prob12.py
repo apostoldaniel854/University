94420c6c6ef71df8e3d0d540069d4968b2704de0
Ilie-Daniel Apostol <47429017+apostoldaniel854@users.noreply.github.com>
2022-10-19 15:40:48 +0300

    Update prob12.py

--- First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob12.py
+++ First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob12.py
@@ -1,10 +1,3 @@
 n = int(input("n="))
 a = list(map(int, input().split()))
-maxValue = a[0]
-minValue = a[0]
-for i in range(1, n):
-    if (maxValue < a[i]):
-        maxValue = a[i]
-    elif (minValue > a[i]):
-        minValue = a[i]
-print(minValue, maxValue)
+print(min(a), max(a))
