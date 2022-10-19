5ee496e945243c1f9cd2ee60ff6e7d2833b60c7e
Ilie-Daniel Apostol <47429017+apostoldaniel854@users.noreply.github.com>
2022-10-19 14:31:16 +0300

    Update prob4.py

--- First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob4.py
+++ First Year/First Semester/Programarea algoritmilor/Laboratoare/Lab 1/prob4.py
@@ -1,4 +1,4 @@
 x = int(input("x="))
 n = int(input("n="))
-x = ((x & ((1 << (n - 1)) - 1)) | ((x >> n) << (n - 1)))
+x = ((x & ((1 << n) - 1)) | ((x >> (n + 1)) << n))
 print("Numarul determinat prin stergerea bitului", n, "este", x)
