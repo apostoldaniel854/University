9b096dcfaa16a4c9a96256b968f37f59a7626ca8
Ilie-Daniel Apostol <47429017+apostoldaniel854@users.noreply.github.com>
2022-10-21 11:26:20 +0300

    Create main.pl

---
+++ First Year/First Semester/Logica matematica si computationala/Laboratoare/Lab 3 - Prolog/main.pl
@@ -0,0 +1,29 @@
+%checking if a number is a palindrome
+rev([], []).
+rev([H|Tailx], M) :- rev(Tailx, U), append(U, [H], M).
+palindrome(L) :- rev(L, L).
+
+%removing duplicates from a list
+remove_duplicates([], []).
+remove_duplicates([H|T], U) :- remove_duplicates(T, U), member(H, U).
+remove_duplicates([H|T], [H|U]) :- remove_duplicates(T, U), not(member(H, U)).
+
+%find if a number can be found a certain amount of times in the list
+atimes(_, [], 0).
+atimes(X, [H|T], W) :- atimes(X, T, Y), X =:= H, W is Y + 1.
+atimes(X, [H|T], Y) :- atimes(X, T, Y), not(X =:= H).
+
+%insertion sort
+%L1 is sorted
+insert(X, [], [X]).
+insert(X, [H|T],[X|[H|T]]) :- X < H.
+insert(X, [H|T], [H|M]) :- X >= H, insert(X, T, M).
+insertsort([],[]).
+insertsort([H|T],L) :- insertsort(T,L1), insert(H,L1,L).
+
+%quicksort
+split(_, [], [], []).
+split(X, [H|T], [H|A], B) :- X > H, split(X, T, A, B).
+split(X, [H|T], A, [H|B]) :- X =< H, split(X, T, A, B).
+quicksort([],[]).
+quicksort([H|T],L) :-split(H,T,A,B), quicksort(A,M), quicksort(B,N), append(M,[H|N],L).
