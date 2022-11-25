97583e817c6bd121e8e8e08a7dee8be51e366f1c
Ilie-Daniel Apostol <47429017+apostoldaniel854@users.noreply.github.com>
2022-11-25 10:47:10 +0200

    Update main.pl

--- First Year/First Semester/Logica matematica si computationala/Laboratoare/Lab 5 - Prolog/main.pl
+++ First Year/First Semester/Logica matematica si computationala/Laboratoare/Lab 5 - Prolog/main.pl
@@ -18,3 +18,18 @@ vars(sau(A, B), L) :- vars(A, L1), vars(B, L2), union(L1, L2, L).
 vars(imp(A, B), L) :- vars(A, L1), vars(B, L2), union(L1, L2, L).
 
 val(V, E, A) :- member((V, A), E).
+
+bnon(0, 1).
+bnon(1, 0).
+bsi(1, 1, 1).
+bsi(1, 0, 0).
+bsi(0, 1, 0).
+bsi(0, 0, 0).
+bimp(1, 0, 0).
+bimp(1, 1, 1).
+bimp(0, 0, 1).
+bimp(0, 1, 1).
+bsau(1, 1, 1).
+bsau(1, 0, 1).
+bsau(0, 1, 1).
+bsau(0, 0, 0).
